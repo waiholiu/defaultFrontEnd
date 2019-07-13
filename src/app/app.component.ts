0f0341599e82fae5aed315b7b11a68e76e6fbf86
waiholiu <waiholiu>
2019-07-13 15:20:31 +1000

    clean up a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-
 
   constructor(private afa: AngularFireAuth, private http: HttpClient) {
-    this.afa.authState.subscribe(this.firebaseAuthChangeListener.bind(this));
     this.afa.idToken.subscribe((data) => {
       console.log(data);
       this.callServer(data);
@@ -27,22 +24,6 @@ export class AppComponent {
   onLogOut(){
     this.afa.auth.signOut();
   }
-
-
-  firebaseAuthChangeListener(response) {
-    // if needed, do a redirect in here
-    if (response) {
-      console.log('Logged in :)');
-
-
-
-      // this.callServer(token);
-
-    } else {
-      console.log('Logged out :(');
-    }
-  }
-
 
   private callServer(token :string) {
     const headers = new HttpHeaders().set("Authorization", "Bearer " + token);
