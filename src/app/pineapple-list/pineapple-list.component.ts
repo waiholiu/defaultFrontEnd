c73bfcb1c528300d030d779a318d0d7426919411
waiholiu <waiholiu>
2019-08-11 21:25:59 +1000

    consuming delete mutation

--- src/app/pineapple-list/pineapple-list.component.ts
+++ src/app/pineapple-list/pineapple-list.component.ts
@@ -3,6 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { Pineapples, Pineapple } from '../models/pineapples';
 import gql from 'graphql-tag';
 
+
+const gqlDeletePineapple = gql`
+mutation($deleteId: Int ){
+  deletePineapple(pineappleId: $deleteId)
+}`;
+
+const gqlGetPineappleList = gql`
+{
+  pineapples{
+    id
+    name
+  }
+}`;
+
+
 @Component({
   selector: 'app-pineapple-list',
   templateUrl: './pineapple-list.component.html',
@@ -15,27 +30,39 @@ export class PineappleListComponent implements OnInit {
   pineapples: Pineapple[];
   loading = true;
   error: any;
-  
+
+
+  async deletePineapple(pineappleId) {
+    const result = await this.apollo.mutate({
+      mutation: gqlDeletePineapple,
+      variables: {
+        deleteId: pineappleId
+      }
+    }).subscribe({
+      next: () => this.getPineappleList(),
+      error: (error) => console.log(error)
+    });
+
+  }
+
+
   ngOnInit() {
 
+    this.getPineappleList();
+
+  }
+
+
+  private getPineappleList() {
     this.apollo
       .watchQuery<Pineapples>({
-
-        query: gql`
-        {
-          pineapples{
-            id
-            name
-          }
-        }
-      `,
+        query: gqlGetPineappleList
+        , fetchPolicy: 'network-only'
       })
       .valueChanges.subscribe(result => {
         this.pineapples = result.data && result.data.pineapples;
         this.loading = result.loading;
         this.error = result.errors;
       });
-
   }
-
 }
